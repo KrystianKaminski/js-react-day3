f1dacda52b99e91d9e136c93f8e5d29b6efb0470
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-21 13:49:53 +0100

    Change function to class

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -4,22 +4,25 @@ import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 
 
-
-const Navigation = (props) => (
-    <div>
-        <AppBar
-            title="Twoja kaloria"
-            onLeftIconButtonClick = {() => alert('Click')}
-
-        />
-        <Drawer
-          docked={false}
-          width={200}
-          open={true}
-          onRequestChange={(open) =>console.log(open)}
-        >
-        </Drawer>
-    </div>
-)
+class Navigation extends React.Component {
+    
+    render() {
+        return ( 
+        <div>
+            <AppBar
+                title="Twoja kaloria"
+                onLeftIconButtonClick = {() => alert('Click')}
+            />
+            <Drawer
+            docked={false}
+            width={200}
+            open={true}
+            onRequestChange={(open) =>console.log(open)}
+            >
+            </Drawer>
+        </div>
+    )
+    }
+}
 
 export default Navigation
