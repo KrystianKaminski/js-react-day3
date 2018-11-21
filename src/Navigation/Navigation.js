824404f5be1424cf19ab1a54aa1d1167726bdfa4
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-21 13:31:20 +0100

    Added page navigation

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import AppBar from 'material-ui/AppBar';
+
+
+const Navigation = (props) => (
+    <div>
+        <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+    </div>
+)
+
+export default Navigation
