0c84851bc4fd8fe204c8c5d45f521ef4f235c8a9
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-21 14:03:55 +0100

    Sidebar created, add event open-closing on button

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -6,25 +6,27 @@ import Drawer from 'material-ui/Drawer';
 
 class Navigation extends React.Component {
     state = {
-        isDrawerOpen: true
+        isDrawerOpen: false
     }
+
+    toggleDrawer = () => this.setState( { isDrawerOpen: !this.state.isDrawerOpen} )
     
     render() {
-        return ( 
-        <div>
-            <AppBar
-                title="Twoja kaloria"
-                onLeftIconButtonClick = {() => alert('Click')}
-            />
-            <Drawer
-            docked={false}
-            width={200}
-            open={this.state.isDrawerOpen}
-            onRequestChange={(open) =>console.log(open)}
-            >
-            </Drawer>
-        </div>
-    )
+        return (
+            <div>
+                <AppBar
+                    title="Twoja kaloria"
+                    onLeftIconButtonClick = {this.toggleDrawer}
+                />
+                <Drawer
+                docked={false}
+                width={200}
+                open={this.state.isDrawerOpen}
+                onRequestChange={this.toggleDrawer}
+                >
+                </Drawer>
+            </div>
+        )
     }
 }
 
