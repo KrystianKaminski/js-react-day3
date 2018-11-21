4fc6308773adb6edd68a65c317d8f14aaa3f24a8
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-21 13:35:57 +0100

    Added button with listener

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -6,8 +6,10 @@ import AppBar from 'material-ui/AppBar';
 const Navigation = (props) => (
     <div>
         <AppBar
-            title="Title"
+            title="Twoja kaloria"
+            onLeftIconButtonClick = {() => alert('Click')}
             iconClassNameRight="muidocs-icon-navigation-expand-more"
+
         />
     </div>
 )
